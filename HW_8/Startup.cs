4b00e55d486a49094e41eef973e64e8a2b50bda1
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2020-10-12 15:15:10 -0600

    starting havnt scaffoled

--- HW_8/Startup.cs
+++ HW_8/Startup.cs
@@ -23,6 +23,7 @@ namespace HW_8
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +49,22 @@ namespace HW_8
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+        private static string convertUrlConnectionString(string url)
+        {
+            if (url is null)
+                throw new ArgumentNullException("It appears you're missing the DATABASE_URL configuration value...");
+            if (!url.Contains("//"))
+                return url;
+            var uri = new Uri(url);
+            var host = uri.Host;
+            var port = uri.Port;
+            var database = uri.Segments.Last();
+            var parts = uri.AbsoluteUri.Split(':', '/', '@');
+            var user = parts[3];
+            var password = parts[4];
+
+            return $"host={host}; port={port}; database={database}; username={user}; password={password}; SSL Mode=Prefer; Trust Server Certificate=true";
         }
     }
 }
